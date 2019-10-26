1b324a8c6dd0c3e728283d030a7593a7277474fd
c-bata <contact@c-bata.link>
2019-10-26 20:27:24 +0900

    update index

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -30,9 +30,9 @@ CREATE TABLE `items` (
   `category_id` int unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  INDEX idx_category_id (`category_id`),
-  INDEX idx_seller_id (`seller_id`),
-  INDEX idx_buyer_id (`buyer_id`),
+  INDEX idx_seller_id_created_at (seller_id, created_at),
+  INDEX idx_category_id_created_at (category_id, created_at),
+  INDEX idx_buyer_id_created_at (buyer_id, created_at),
   INDEX idx_created_at (`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
