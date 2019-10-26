9c8a6d2fb0ccd4f5411d71b2705d72657dc13dbd
c-bata <contact@c-bata.link>
2019-10-26 05:42:26 +0000

    Add reload script

---
+++ reload-local.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -ex
+
+cd ~/isucari
+
+cd ./webapp/go/
+make all
+cd ~/isucari
+
+# log rotate
+if [ -f /var/log/nginx/access.log ]; then
+     sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$(date +"%Y%m%d_%H%M%S")
+fi
+if [ -f /var/log/mysql/mysql-slow.log ]; then
+    sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.$(date +"%Y%m%d_%H%M%S")
+fi
+
+# service restart
+sudo systemctl restart isucari.golang.service
+sudo systemctl restart nginx.service
+sudo systemctl restart mysql.service
+echo "Success to restart!"
+
+# mysql setting
+# echo "set global slow_query_log = ON;" | sudo mysql -u root
+# echo "set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';" | sudo mysql -u root
+# echo "set global long_query_time = 0;" | sudo mysql -u root
+# echo "Success slow_query_log set!"
+
+# init private dir
+rm -rf /home/isucon/isucari/webapp/private
+mkdir -p /home/isucon/isucari/webapp/private/qrcode
+
+echo "Success to reload!"
+
